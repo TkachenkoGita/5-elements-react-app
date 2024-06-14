a3ab3cec7ef2a095f8af1e8a9851640129629325
Hita <tkachenkohita@gmail.com>
2024-06-14 14:01:07 +0300

    modal-structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-
-import { useState } from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
+import ModalWindow from "./components/ModalWindow";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const decCount = () => {
-    setCount(count-1)
-  }
-  const incCount = () => {
-    setCount(count + 1);
-  };
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>{count}</h1>
-        <button onClick={decCount} className="minus">- Минус</button>
-        <button onClick={incCount} className="plus">Плюс +</button>
-      </div>
+      <button className="open-modal-btn">✨ Открыть окно</button>
+      <ModalWindow/>
     </div>
   );
 }
