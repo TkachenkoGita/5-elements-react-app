e0b86ec0a228022d5ae14568db6722237da73ab6
Hita <tkachenkohita@gmail.com>
2024-06-14 14:11:34 +0300

    conditional operator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import ModalWindow from "./components/ModalWindow";
+//условный рендеринг
 
 function App() {
+  const [open, setOpen] = useState(false)
+  
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      <ModalWindow/>
+      <button onClick={()=>setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
+      {open && <ModalWindow setOpen={setOpen} />}
     </div>
   );
 }
--- src/components/ModalWindow/index.jsx
+++ src/components/ModalWindow/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function ModalWindow() {
+function ModalWindow(props) {
+  const { setOpen } = props;
   return (
     <div className="overlay">
       <div className="modal">
-        <svg height="200" viewBox="0 0 200 200" width="200">
+        <svg onClick={()=>setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
           <title />
           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
         </svg>
