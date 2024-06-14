59759b913a03158d4175f3fbabbd44bda0148769
Hita <tkachenkohita@gmail.com>
2024-06-14 14:34:40 +0300

    quiz-component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import Game from "./components/Game";
 import "./index.scss";
-import ModalWindow from "./components/ModalWindow";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
 
 
 function App() {
-  const [open, setOpen] = useState(false)
-  
   return (
     <div className="App">
-      <button onClick={()=>setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-       <ModalWindow setOpen={setOpen} open={open} />
+      <Game />
+      {/* <Result /> */}
     </div>
   );
 }
